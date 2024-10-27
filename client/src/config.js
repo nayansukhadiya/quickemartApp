ed43086dfcc2849604d3936fd53f38d56cd6013b
Nayan313 <nayansukhadiya3105@gmail.com>
2024-10-27 18:07:24 +0530

    code

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiUrl: "quickemart-icnx-q8i1k85sc-nayan313s-projects.vercel.app",
+  apiUrl: "http://localhost:5000",
 };
 // 'http://localhost:5000'
 // process.env.REACT_APP_BACKEND_URL

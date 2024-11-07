501ab17f988e9748b4f021907a3c956f13e393d1
Nayan313 <nayansukhadiya31@gmail.com>
2024-11-07 15:34:56 +0530

    code

--- client/src/Layout.js
+++ client/src/Layout.js
@@ -53,11 +53,9 @@ function Layout() {
   useEffect(()=> {
     const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
     if (isMobile) { 
-      setSearchBarRem(false);
-    }else if(!isMobile){
-      setSearchBarRem(false);
-    } else {
       setSearchBarRem(true);
+    } else {
+      setSearchBarRem(false);
     }
   }, [location])
 
